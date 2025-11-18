f7efef55309b3ef4d9cb6cb1682b5988236bd888
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-18 02:15:12 -0800

    Started work on using MIRAI

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![cfg(mirai)]
+#![feature(let_chains)]
+
 // TODO: Use a formal verifier (such as Creusot, Kani, Verus, etc...) once one of them supports enough of the language features
 pub mod dependent;
 /// WIP
