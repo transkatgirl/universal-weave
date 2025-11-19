938ed33c00ab7a2eecc7c2ddf0e4bd750f952b3c
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-19 02:56:04 -0800

    Bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(mirai, feature(let_chains))]
+
 // TODO: Use a formal verifier (such as Creusot, Kani, Verus, etc...) once one of them supports enough of the language features
 pub mod dependent;
 /// WIP
