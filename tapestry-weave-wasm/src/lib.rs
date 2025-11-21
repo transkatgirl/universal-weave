70202128e657958b4f37cc4425127b1e6ab7cb7f
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-21 01:59:53 -0800

    Code cleanup

--- tapestry-weave-wasm/src/lib.rs
+++ tapestry-weave-wasm/src/lib.rs
@@ -1,10 +1,9 @@
+mod identifiers;
 mod utils;
 
-use rand::Rng;
 use serde::{Deserialize, Serialize};
 use tapestry_weave::{ulid::Ulid, universal_weave::indexmap::IndexSet};
 use wasm_bindgen::prelude::*;
-use web_time::{Duration, Instant, SystemTime};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Node {
@@ -13,27 +12,6 @@ pub struct Node {
     to: Vec<u128>,
     active: bool,
     bookmarked: bool,
-}
-
-#[wasm_bindgen]
-pub fn new_identifier() -> u128 {
-    let timestamp_ms = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap_or(Duration::ZERO)
-        .as_millis();
-    let rng = &mut rand::rng();
-
-    Ulid::from_parts(timestamp_ms as u64, rng.random()).0
-}
-
-#[wasm_bindgen]
-pub fn get_unix_time_ms_from_identifier(value: u128) -> u64 {
-    Ulid(value).timestamp_ms()
-}
-
-#[wasm_bindgen]
-pub fn identifier_to_string(value: u128) -> String {
-    Ulid(value).to_string()
 }
 
 //#[wasm_bindgen]
