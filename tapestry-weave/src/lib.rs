d9fa99b993ab77a90e2fca00f25250c9181efc73
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-11-23 05:44:33 -0800

    Updated TODOs

--- tapestry-weave/src/lib.rs
+++ tapestry-weave/src/lib.rs
@@ -50,5 +50,6 @@ impl VersionedWeave {
 //   - Implement diff-based tree updates
 //   - Implement prefix-based deduplication?
 //   - Implement support for editor undo/redo
+//   - Implement event-based invalidation support for multi-user weaves
 
 // Useful reference for future v1 format: https://github.com/transkatgirl/Tapestry-Loom/blob/a232fbbb4119a8a9047ca67a8f1b0cfb772c5bb1/weave/src/document/content/mod.rs
