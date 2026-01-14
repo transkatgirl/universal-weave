aaa6093f9e145eeeca4a47b1f2e4298825575b3f
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-01-13 21:44:38 -0800

    Added versioning convenience functions, updated dependencies and TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,20 @@
 // TODO: Unit tests
 // TODO: Use a formal verifier (such as Creusot, Kani, Verus, etc...) once one of them supports enough of the language features
 
+// Planned API tweaks:
+// - Add associated type: Scratchpad
+// - Allow the following functions to take in a &mut Vec<T>
+//    - get_ordered_node_identifiers()
+//    - get_ordered_node_identifiers_reversed_children()
+//    - get_active_thread()
+//    - get_thread_from()
+// - Ensure that &mut self is only used when absolutely necessary
+
 pub mod dependent;
 pub mod independent;
+
+#[cfg(feature = "rkyv")]
+pub mod versioning;
 
 use std::{
     cmp::Ordering,
---
+++ src/versioning.rs
@@ -0,0 +1,42 @@
+//! Utilities for versioning serialized binary data
+
+/// A set of bytes accompanied by file header information
+pub struct VersionedBytes<'a> {
+    /// The magic bytes at the start of the file indicating the format used
+    pub format_identifier: [u8; 24],
+    /// The format version stored within the header
+    pub version: u64,
+    /// The data following the header
+    pub data: &'a [u8],
+}
+
+impl<'a> VersionedBytes<'a> {
+    /// Tries to deserialize a [`VersionedBytes`] struct from a byte array
+    ///
+    /// This can fail in falling cases:
+    /// - The specified `format_identifier` does not match the first 24 bytes of the byte array
+    /// - The byte array is less than 32 bytes long
+    pub fn try_from_bytes(value: &'a [u8], format_identifier: [u8; 24]) -> Option<Self> {
+        if value.starts_with(&format_identifier) && value.len() >= 32 {
+            let (version_bytes, data) = value[24..].split_at(8);
+
+            Some(Self {
+                format_identifier,
+                version: u64::from_le_bytes(version_bytes.try_into().unwrap()),
+                data,
+            })
+        } else {
+            None
+        }
+    }
+    /// The total length in bytes after serialization
+    pub fn output_length(&self) -> usize {
+        32 + self.data.len()
+    }
+    /// Serializes the header and contents into the specified byte array
+    pub fn to_bytes(self, output: &mut Vec<u8>) {
+        output.extend(self.format_identifier);
+        output.extend(self.version.to_le_bytes());
+        output.extend(self.data);
+    }
+}
