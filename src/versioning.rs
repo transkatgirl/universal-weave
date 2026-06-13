d69cc2a2c4ec2d7dcdd6db727516e164ef9f87fd
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-06-13 08:07:27 -0700

    Improved versioning API

--- src/versioning.rs
+++ src/versioning.rs
@@ -1,6 +1,9 @@
 //! Utilities for versioning serialized binary data
 
+use std::io::{self, Write};
+
 /// A set of bytes accompanied by file header information
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct VersionedBytes<'a> {
     /// The magic bytes at the start of the file indicating the format used
     pub format_identifier: [u8; 24],
@@ -33,10 +36,20 @@ impl<'a> VersionedBytes<'a> {
     pub fn output_length(&self) -> usize {
         32 + self.data.len()
     }
-    /// Serializes the header and contents into the specified byte array
-    pub fn to_bytes(self, output: &mut Vec<u8>) {
-        output.extend(self.format_identifier);
-        output.extend(self.version.to_le_bytes());
-        output.extend(self.data);
+    /// Returns the serialized header bytes
+    pub fn header_bytes(&self) -> [u8; 32] {
+        let mut header: [u8; 32] = [0; 32];
+        header[..24].copy_from_slice(&self.format_identifier);
+        header[24..].copy_from_slice(&self.version.to_le_bytes());
+
+        header
+    }
+    /// Serializes the header and contents into the specified writer
+    pub fn write_as_bytes(&self, output: &mut impl Write) -> Result<(), io::Error> {
+        output.write_all(&self.format_identifier)?;
+        output.write_all(&self.version.to_le_bytes())?;
+        output.write_all(self.data)?;
+
+        Ok(())
     }
 }
