ff9e888089ae82d7ccaeed4150cd33bf4bb70309
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-07-27 03:24:26 -0700

    Further work on traversal

--- src/dependent/loro/mod.rs
+++ src/dependent/loro/mod.rs
@@ -19,7 +19,6 @@ use rkyv::{
     to_bytes,
     util::AlignedVec,
 };
-use stacksafe::stacksafe;
 
 use crate::{
     ActiveSingularWeave, BookmarkableWeave, DeduplicatableContents, DeduplicatableWeave,
@@ -443,72 +442,6 @@ where
     }
 }
 
-impl<K, T, M, S> DependentLoroWeave<K, T, M, S>
-where
-    for<'a> K: Archive
-        + Serialize<HighSerializer<AlignedVec, ArenaHandle<'a>, rancor::Error>>
-        + Hash
-        + Copy
-        + Eq,
-    for<'a> K::Archived: CheckBytes<HighValidator<'a, rancor::Error>>
-        + Deserialize<K, Strategy<Pool, rancor::Error>>,
-    for<'a> T: Archive + Serialize<HighSerializer<AlignedVec, ArenaHandle<'a>, rancor::Error>> + Eq,
-    for<'a> T::Archived: CheckBytes<HighValidator<'a, rancor::Error>>
-        + Deserialize<T, Strategy<Pool, rancor::Error>>,
-    for<'a> M: Archive + Serialize<HighSerializer<AlignedVec, ArenaHandle<'a>, rancor::Error>> + Eq,
-    for<'a> M::Archived: CheckBytes<HighValidator<'a, rancor::Error>>
-        + Deserialize<M, Strategy<Pool, rancor::Error>>,
-    S: BuildHasher + Default + Clone,
-{
-    #[stacksafe]
-    fn validate_subtree(
-        &self,
-        tree: &LoroTree,
-        target: TreeID,
-        parent: Option<K>,
-        buffer: &mut AlignedVec,
-        counter: &mut usize,
-    ) -> bool {
-        if let Ok(meta) = tree.get_meta(target)
-            && let Some(ValueOrContainer::Value(LoroValue::Binary(binary_id))) = meta.get("id")
-            && let Some(ValueOrContainer::Value(LoroValue::Binary(binary_contents))) =
-                meta.get("contents")
-            && let Ok(id) = from_bytes_aligned(&binary_id, buffer)
-            && let Ok(contents) = from_bytes_aligned(&binary_contents, buffer)
-            && let Some(node) = self.weave.get_node(&id)
-            && node.from == parent
-            && node.contents == contents
-        {
-            *counter = counter.strict_add(1);
-
-            let children = tree.children(target).unwrap_or_default();
-
-            if node.to.len() != children.len() {
-                return false;
-            }
-
-            for (index, child) in children.iter().enumerate() {
-                if let Some(at_index) = node.to.get_index(index)
-                    && self.mapping.get(at_index) == Some(child)
-                {
-                } else {
-                    return false;
-                }
-            }
-
-            for child in children {
-                if !self.validate_subtree(tree, child, Some(id), buffer, counter) {
-                    return false;
-                }
-            }
-
-            true
-        } else {
-            false
-        }
-    }
-}
-
 fn from_bytes_aligned<T, E>(bytes: &[u8], buffer: &mut AlignedVec) -> Result<T, E>
 where
     T: Archive,
@@ -790,13 +723,50 @@ where
             }
         }
 
-        let mut counter = 0;
+        let mut counter: usize = 0;
 
         for (index, root) in tree.roots().into_iter().enumerate() {
             if let Some(at_index) = self.weave.roots.get_index(index)
                 && self.mapping.get(at_index) == Some(&root)
-                && self.validate_subtree(&tree, root, None, &mut buffer, &mut counter)
             {
+                let mut stack = Vec::with_capacity(self.weave.len());
+
+                stack.push((root, None));
+
+                while let Some((target, parent)) = stack.pop() {
+                    if let Ok(meta) = tree.get_meta(target)
+                        && let Some(ValueOrContainer::Value(LoroValue::Binary(binary_id))) =
+                            meta.get("id")
+                        && let Some(ValueOrContainer::Value(LoroValue::Binary(binary_contents))) =
+                            meta.get("contents")
+                        && let Ok(id) = from_bytes_aligned(&binary_id, &mut buffer)
+                        && let Ok(contents) = from_bytes_aligned(&binary_contents, &mut buffer)
+                        && let Some(node) = self.weave.get_node(&id)
+                        && node.from == parent
+                        && node.contents == contents
+                    {
+                        counter = counter.strict_add(1);
+
+                        let children = tree.children(target).unwrap_or_default();
+
+                        if node.to.len() != children.len() {
+                            return false;
+                        }
+
+                        for (index, child) in children.iter().enumerate() {
+                            if let Some(at_index) = node.to.get_index(index)
+                                && self.mapping.get(at_index) == Some(child)
+                            {
+                            } else {
+                                return false;
+                            }
+                        }
+
+                        stack.extend(children.into_iter().rev().map(|child| (child, Some(id))));
+                    } else {
+                        return false;
+                    }
+                }
             } else {
                 return false;
             }
