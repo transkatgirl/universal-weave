cf8f72a0792a0584ee41356a4fd15d227ca85793
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-08-13 23:17:02 -0700

    Tweaked scaffolding

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -5,7 +5,10 @@
 //! - [`IndependentLayouter`] - Takes an [`IndependentWeave`] as an input.
 //! - [`TopologicalLayouter`] - Takes any [`Weave`] as an input.
 
-use core::hash::{BuildHasher, Hash};
+use core::{
+    hash::{BuildHasher, Hash},
+    marker::PhantomData,
+};
 
 use glam::Vec2;
 use scratchpads::Scratchpad;
@@ -16,6 +19,8 @@ use crate::{
     independent::{IndependentNode, IndependentWeave},
 };
 
+mod positioner;
+
 /// Minimum gaps in a [`Weave`] layout.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 #[must_use]
@@ -24,42 +29,69 @@ pub struct Spacing {
     pub node: f32,
     /// Gap between layers of nodes.
     pub layer: f32,
-    /// Gap between node connectors.
-    pub connections: f32,
+    /// Gap between node connections.
+    pub edge: f32,
 }
-
-/// A 2D arrangement of a [`Weave`]'s contents.
-#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Layout2D {
-    // TODO
-}
-
-/// An error which occured while attempting to build a [`Layout2D`].
-pub enum LayoutError {}
 
 impl Default for Spacing {
     fn default() -> Self {
         Self {
             node: 16.0,
             layer: 16.0,
-            connections: 8.0,
+            edge: 8.0,
         }
     }
 }
+
+/// A 2D arrangement of a [`Weave`]'s content.
+#[derive(Debug, Clone)]
+#[must_use]
+pub struct Layout2D<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
+    _k: PhantomData<K>,
+    _s: PhantomData<S>,
+}
+
+impl<K, S> Default for Layout2D<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
+    fn default() -> Self {
+        Self {
+            _k: PhantomData,
+            _s: PhantomData,
+        }
+    }
+}
+
+/// An error which occured while attempting to build a [`Layout2D`].
+pub enum LayoutError {}
 
 /// A 2D [`Layouter`] which takes a [`DependentWeave`] as input.
 ///
 /// This layout algorithm has identical behavior to [`TopologicalLayouter`].
 #[derive(Default, Debug, Clone)]
 #[must_use]
-pub struct DependentLayouter {
+pub struct DependentLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     /// The [`Spacing`] used to arrange contents.
     pub spacing: Spacing,
 
-    layout: Layout2D,
+    layout: Layout2D<K, S>,
 }
 
-impl DependentLayouter {
+impl<K, S> DependentLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     /// Creates a new [`DependentLayouter`] with the specified spacing.
     pub fn new(spacing: Spacing) -> Self {
         Self {
@@ -70,13 +102,13 @@ impl DependentLayouter {
 }
 
 impl<'a, K, T, M, S> Layouter<'a, DependentWeave<K, T, M, S>, K, DependentNode<K, T, S>, T>
-    for DependentLayouter
+    for DependentLayouter<K, S>
 where
-    K: Hash + Copy + Eq + Ord,
-    S: BuildHasher + Default + Clone,
+    K: Hash + Copy + Eq + Ord + 'static,
+    S: BuildHasher + Default + Clone + 'static,
 {
     type Size = Vec2;
-    type Layout = &'a Layout2D;
+    type Layout = &'a Layout2D<K, S>;
     type Error = LayoutError;
 
     fn layout(
@@ -84,6 +116,8 @@ where
         weave: &mut DependentWeave<K, T, M, S>,
         map: impl FnMut(&DependentNode<K, T, S>) -> Self::Size,
     ) -> Result<Self::Layout, Self::Error> {
+        let scratchpad = &mut weave.scratchpad;
+
         todo!()
     }
 }
@@ -93,14 +127,22 @@ where
 /// This layout algorithm has identical behavior to [`TopologicalLayouter`].
 #[derive(Default, Debug, Clone)]
 #[must_use]
-pub struct IndependentLayouter {
+pub struct IndependentLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     /// The [`Spacing`] used to arrange contents.
     pub spacing: Spacing,
 
-    layout: Layout2D,
+    layout: Layout2D<K, S>,
 }
 
-impl IndependentLayouter {
+impl<K, S> IndependentLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     /// Creates a new [`IndependentLayouter`] with the specified spacing.
     pub fn new(spacing: Spacing) -> Self {
         Self {
@@ -111,14 +153,14 @@ impl IndependentLayouter {
 }
 
 impl<'a, K, T, M, S> Layouter<'a, IndependentWeave<K, T, M, S>, K, IndependentNode<K, T, S>, T>
-    for IndependentLayouter
+    for IndependentLayouter<K, S>
 where
-    K: Hash + Copy + Eq + Ord,
+    K: Hash + Copy + Eq + Ord + 'static,
     T: IndependentContents,
-    S: BuildHasher + Default + Clone,
+    S: BuildHasher + Default + Clone + 'static,
 {
     type Size = Vec2;
-    type Layout = &'a Layout2D;
+    type Layout = &'a Layout2D<K, S>;
     type Error = LayoutError;
 
     fn layout(
@@ -126,6 +168,8 @@ where
         weave: &mut IndependentWeave<K, T, M, S>,
         map: impl FnMut(&IndependentNode<K, T, S>) -> Self::Size,
     ) -> Result<Self::Layout, Self::Error> {
+        let scratchpad = &mut weave.scratchpad;
+
         todo!()
     }
 }
@@ -135,15 +179,23 @@ where
 /// However, this additional flexibility may result in worse performance and memory usage characteristics compared to [`DependentLayouter`] or [`IndependentLayouter`].
 #[derive(Debug, Clone)]
 #[must_use]
-pub struct TopologicalLayouter {
+pub struct TopologicalLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     /// The [`Spacing`] used to arrange contents.
     pub spacing: Spacing,
 
-    layout: Layout2D,
+    layout: Layout2D<K, S>,
     scratchpad: Scratchpad,
 }
 
-impl Default for TopologicalLayouter {
+impl<K, S> Default for TopologicalLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     fn default() -> Self {
         Self {
             spacing: Spacing::default(),
@@ -153,7 +205,11 @@ impl Default for TopologicalLayouter {
     }
 }
 
-impl TopologicalLayouter {
+impl<K, S> TopologicalLayouter<K, S>
+where
+    K: Hash + Copy + Eq + Ord,
+    S: BuildHasher + Default + Clone,
+{
     /// Creates a new [`TopologicalLayouter`] with the specified spacing.
     pub fn new(spacing: Spacing) -> Self {
         Self {
@@ -164,14 +220,15 @@ impl TopologicalLayouter {
     }
 }
 
-impl<'a, W, K, N, T> Layouter<'a, W, K, N, T> for TopologicalLayouter
+impl<'a, W, K, N, T, S> Layouter<'a, W, K, N, T> for TopologicalLayouter<K, S>
 where
     W: Weave<K, N, T>,
-    K: Hash + Copy + Eq + Ord,
+    K: Hash + Copy + Eq + Ord + 'static,
     N: Node<K, T>,
+    S: BuildHasher + Default + Clone + 'static,
 {
     type Size = Vec2;
-    type Layout = &'a Layout2D;
+    type Layout = &'a Layout2D<K, S>;
     type Error = LayoutError;
 
     fn layout(
