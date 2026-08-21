e8cd361ca12e067a9e48bf0a2894eabc4839fc5a
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-08-20 17:52:50 -0700

    Updated feature checks

--- tests/features.sh
+++ tests/features.sh
@@ -1,5 +1,6 @@
-cargo build --target=aarch64-unknown-none --no-default-features --features rkyv,serde,legacy
-cargo build --release --target=aarch64-unknown-none --no-default-features --features rkyv,serde,legacy
+#cargo doc --features layout,rkyv,serde,legacy
+cargo build --target=aarch64-unknown-none --no-default-features --features layout,rkyv,serde,legacy
+cargo build --release --target=aarch64-unknown-none --no-default-features --features layout,rkyv,serde,legacy
 cargo clippy --no-default-features
 cargo clippy --no-default-features --features rkyv
 cargo clippy --no-default-features --features serde
@@ -8,6 +9,14 @@ cargo clippy --no-default-features --features rkyv,serde
 cargo clippy --no-default-features --features rkyv,legacy
 cargo clippy --no-default-features --features serde,legacy
 cargo clippy --no-default-features --features rkyv,serde,legacy
+cargo clippy --no-default-features --features layout
+cargo clippy --no-default-features --features layout,rkyv
+cargo clippy --no-default-features --features layout,serde
+cargo clippy --no-default-features --features layout,legacy
+cargo clippy --no-default-features --features layout,rkyv,serde
+cargo clippy --no-default-features --features layout,rkyv,legacy
+cargo clippy --no-default-features --features layout,serde,legacy
+cargo clippy --no-default-features --features layout,rkyv,serde,legacy
 cargo clippy --release --no-default-features
 cargo clippy --release --no-default-features --features rkyv
 cargo clippy --release --no-default-features --features serde
@@ -16,10 +25,19 @@ cargo clippy --release --no-default-features --features rkyv,serde
 cargo clippy --release --no-default-features --features rkyv,legacy
 cargo clippy --release --no-default-features --features serde,legacy
 cargo clippy --release --no-default-features --features rkyv,serde,legacy
+cargo clippy --release --no-default-features --features layout
+cargo clippy --release --no-default-features --features layout,rkyv
+cargo clippy --release --no-default-features --features layout,serde
+cargo clippy --release --no-default-features --features layout,legacy
+cargo clippy --release --no-default-features --features layout,rkyv,serde
+cargo clippy --release --no-default-features --features layout,rkyv,legacy
+cargo clippy --release --no-default-features --features layout,serde,legacy
+cargo clippy --release --no-default-features --features layout,rkyv,serde,legacy
 
 # Loro integration will soon be separated into new crate
-cargo build --no-default-features --features loro,rkyv,serde,legacy
-cargo build --release --no-default-features --features loro,rkyv,serde,legacy
+cargo doc --features loro,layout,rkyv,serde,legacy
+cargo build --no-default-features --features loro,layout,rkyv,serde,legacy
+cargo build --release --no-default-features --features loro,layout,rkyv,serde,legacy
 cargo clippy --no-default-features --features loro
 cargo clippy --no-default-features --features loro,rkyv
 cargo clippy --no-default-features --features loro,serde
