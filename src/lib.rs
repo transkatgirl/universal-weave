d6d5494b193dff85b7d33bd6307861ac05a157ed
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-09-08 03:30:37 -0700

    Tweaked DiscreteContents trait

--- src/lib.rs
+++ src/lib.rs
@@ -173,9 +173,9 @@ pub trait DiscreteContents: Sized {
     fn len(&self) -> usize;
     /// Returns `true` if the item has a length of zero.
     fn is_empty(&self) -> bool;
-    /// Splits the item at specified index.
+    /// Splits the item at the specified index.
     ///
-    /// If splitting the item fails, the original contents are returned.
+    /// If splitting the item at the exact index fails, the original contents are returned.
     fn split(self, at: usize) -> DiscreteContentResult<Self>;
     /// Merges two items together.
     ///
