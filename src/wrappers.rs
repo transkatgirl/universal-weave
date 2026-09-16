1f66a1a0982a2efdc3f20182e12f95e7f1c073d7
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-09-16 12:19:05 -0700

    Tweaked documentation

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -2867,7 +2867,7 @@ where
     ///
     /// If the range extends beyond the active path, its length is clamped to the active path's length.
     ///
-    /// If the range is empty, this function behaves identically to [`Self::insert_at()`].
+    /// If `range` is empty, this function behaves identically to [`Self::insert_at()`].
     ///
     /// This function may split up to 2 nodes and may insert up to 1 additional node if necessary to apply the operation.
     ///
